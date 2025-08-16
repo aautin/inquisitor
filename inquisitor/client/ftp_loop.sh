87869f1e8d7179ef15f89c5e962fbbf15f44d5b4
aautin <alexandreautin.aa@gmail.com>
2025-08-16 13:19:54 +0200

    fix inquisitor module conf (need poison implementation)

--- inquisitor/client/ftp_loop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-while true; do
-    lftp -u anonymous, -e "ls; bye" ftp://$SERVER_IP
-    sleep 3
-done
---
+++ inquisitor/inquisitor/run.sh
@@ -0,0 +1,3 @@
+g++ -o inquisitor src/*.cpp -lpcap
+
+./inquisitor $CLIENT_IP $CLIENT_MAC $SERVER_IP $SERVER_MAC
